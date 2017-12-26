7078aa88e9d03a31455f85eb8e963ac57531a0aa
lcwxjj <1983304914@qq.com>
2017-12-26 09:37:56 +0800

    ngDemo

---
+++ app.js
@@ -0,0 +1,9 @@
+var hello = /** @class */ (function () {
+    function hello() {
+    }
+    return hello;
+}());
+var login = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(login(10, 20));
